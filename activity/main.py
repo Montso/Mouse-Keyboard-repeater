1538b053b9d2cabb39d00a75a221b9f6b487904c
Montsho Mokake <montshomokake@gmail.com>
2021-07-02 05:08:03 +0200

    Updated listener.py, main.py
    listener: logging time format 3dp(after comma)
    
    activity/main.py: *all checked
    *writes time as delta. first log=0, last log=-1.,

--- activity/main.py
+++ activity/main.py
@@ -1,29 +1,34 @@
-import datetime
+from datetime import datetime
 
-temp = open('../temp/log.txt', 'r')
-
-logs = temp.readlines() #store each line as list item
+with open('../temp/log.txt', 'r') as temp:
+		logs = temp.readlines() #store each line as list item
+		temp.close()
 
 logs = [log.split('|') for log in logs] #split each line data: [time, key, evennt]
 
 for log in logs:
 
 	#clean the data
-	log[0] = log[0][2:-4] #time: '2021-07-01 15:11:10,968' => '21-07-01 15:11:10'
+	
 	log[2] = log[2][:-1] #event: 'r\n' => 'r'
 
-	time_str = log[0]
-	time_obj = datetime.strptime(time_str, "%y-%m-%d %H:%M:%S.%f") #string to time object
+	time_str = log[0][2:] #time, omitting first 2 digits of year: yy-mm-dd
+	time_obj = datetime.strptime(time_str, "%y-%m-%d %H:%M:%S,%f") #string to time object
 	log[0] = time_obj
 	log = (log[0], log[1], log[2])
-	print(log)
 
 for log in logs:
 
-	if logs.indexOf(log) == 0:
-		log[0] = 0
-	elif logs.indexOf(log) == len(logs-1):
-		log[0] = -1
+	if logs.index(log) == 0: #flag first event with t=0
+		log[0] = int(0)
+	elif logs.index(log) == (len(logs)-1): #flag first event with t=-1
+		log[0] = int(-1)
 	else:
-		log[0] = logs[logs.indexOf(log)+1][0] - logs[logs.indexOf(log)][0]
-print(logs)
+		log[0] = logs[logs.index(log)+1][0] - logs[logs.index(log)][0]
+		log[0] = log[0].total_seconds()
+	
+logs = [tuple(log) for log in logs]	
+
+file = open('../logs/log.txt', 'w')
+
+for log in logs: file.write("{}\n".format(log))

4184d8f8ff7cf6c3ea41294e94cb33025b81a61d
Montsho Mokake <montshomokake@gmail.com>
2021-07-03 19:59:18 +0200

    Update listener.py, main.py
    listener: export logs as  in dictionary
    main: dictionary string repr conversion to dict with json

--- activity/main.py
+++ activity/main.py
@@ -1,34 +1,39 @@
 from datetime import datetime
+import json
 
 with open('../temp/log.txt', 'r') as temp:
 		logs = temp.readlines() #store each line as list item
 		temp.close()
 
 logs = [log.split('|') for log in logs] #split each line data: [time, key, evennt]
+for log in logs: log[1] = log[1].replace("\'", "\"")[:-1]
+print(logs)
+for log in logs: log[1] = json.loads(log[1])
+print(logs)
 
-for log in logs:
 
-	#clean the data
+# for log in logs:
+
+# 	#clean the data
 	
-	log[2] = log[2][:-1] #event: 'r\n' => 'r'
+# 	log[2] = log[2][:-1] #event: 'r\n' => 'r'
 
-	time_str = log[0][2:] #time, omitting first 2 digits of year: yy-mm-dd
-	time_obj = datetime.strptime(time_str, "%y-%m-%d %H:%M:%S,%f") #string to time object
-	log[0] = time_obj
-	log = (log[0], log[1], log[2])
+# 	time_str = log[0][2:] #time, omitting first 2 digits of year: yy-mm-dd
+# 	time_obj = datetime.strptime(time_str, "%y-%m-%d %H:%M:%S,%f") #string to time object
+# 	log[0] = time_obj
+# 	log = (log[0], log[1], log[2])
 
-for log in logs:
+# for log in logs:
 
-	if logs.index(log) == 0: #flag first event with t=0
-		log[0] = int(0)
-	elif logs.index(log) == (len(logs)-1): #flag first event with t=-1
-		log[0] = int(-1)
-	else:
-		log[0] = logs[logs.index(log)+1][0] - logs[logs.index(log)][0]
-		log[0] = log[0].total_seconds()
+# 	if logs.index(log) == 0: #flag first event with t=0
+# 		log[0] = int(0)
+# 	elif logs.index(log) == (len(logs)-1): #flag first event with t=-1
+# 		log[0] = int(-1)
+# 	else:
+# 		log[0] = logs[logs.index(log)+1][0] - logs[logs.index(log)][0]
+# 		log[0] = log[0].total_seconds()
 	
-logs = [tuple(log) for log in logs]	
+# logs = [tuple(log) for log in logs]	
 
-file = open('../logs/log.txt', 'w')
-
-for log in logs: file.write("{}\n".format(log))
+# file = open('../logs/log.txt', 'w')
+# for log in logs: file.write("{}\n".format(log))
--- listener/listener.py
+++ listener/listener.py
@@ -7,55 +7,92 @@ logging.basicConfig(filename="../temp/log.txt", level=logging.DEBUG, format='%(a
 
 
 
-class mous():
+class Mouse():
 	#Mouse activity listener
 
 
 	def __init__(self):
-		print("mouse")
-	
+		self.obj = "mouse"
+
 	def on_move(self, x, y):
 		print("moved to {}".format((x, y)))
-		logging.debug("{}|{}".format(x,y))
+		event = {
+				"obj": self.obj,
+				"event": "move",
+				"x": x,
+				"y": y
+		        }
+		logging.debug(event)
 
 
 	def on_scroll(self, x, y, dx, dy):
 		print("Scrolled {0} from {1}\n Vector: {2}".format("Down" if dy < 0 else "Up", (x,y), dy))
-		logging.debug("{}|{}|{}".format(x,y,dy,dx))
+		event = {
+				"obj": self.obj,
+				"event": "scroll",
+				"dy": x
+		        }
+		logging.debug(event)
 
 
-class keyB():
+class Keyboard():
 	'''
 	keyboard activity listener
 	'''
 
 	def __init__(self):
-		pass
+		self.obj = "keyboard"
 
 	def on_press(self, key):
 		try:
 			print("key pressed: {0}".format(key.char))
-			logging.debug("{}|{}".format(key.char, 'p'))
+			event = {
+				"obj": self.obj,
+				"event": "keypress",
+				"key": key.char,
+				"status": "p"
+		        }
+			logging.debug(event)
+
 		except:
 			print("Special key pressed: {0}".format(key))
-			logging.debug("{}|{}".format(key, 'p'))
+
+			event = {
+				"obj": self.obj,
+				"event": "keypress",
+				"key": str(key),
+				"status": "p"
+		        }
+			logging.debug(event)
 
 	def on_rel(self, key):
 		try:
 			print("key released: {0}".format(key))
-			logging.debug("{}|{}".format(key.char, 'r'))
+			event = {
+				"obj": self.obj,
+				"event": "release",
+				"key": key.char,
+				"status": "r"
+		        }
+			logging.debug(event)
 		except:
 			print("Special key released: {0}".format(key))
-			logging.debug("{}|{}".format(key, 'r'))
+			event = {
+				"obj": self.obj,
+				"event": "release",
+				"key": str(key),
+				"status": "r"
+		        }
+			logging.debug(event)
 
 
 		#exist on ESC
 		if key == keyboard.Key.esc:
 			return False
 
-keyB = keyB()
-mou = mous()
+keyB = Keyboard()
+mous = Mouse()
 with keyboard.Listener(on_press=keyB.on_press, on_release=keyB.on_rel) as listner:
-	with mouse.Listener(on_move=mou.on_move, on_scroll=mou.on_scroll) as listner:
+	with mouse.Listener(on_move=mous.on_move, on_scroll=mous.on_scroll) as listner:
 		listner.join()
 
